20bab295d01cea7b5141e53e21c65cf0a297ccc9
James Spargo <solacedforever@gmail.com>
2017-07-24 16:14:19 -0400

    changes

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const chalk = require('chalk');
 var moment = require('moment');
 // moment().format();
-let line4 = "";
-
 if (moment().isDST()) {
     line4 = "is"
 }else{
     line4 = "is not";
 }
-
-  let line5 = "";
-  if (moment().isLeapYear()){
+if (moment().isLeapYear()){
     line5 = "is";
   }else{
     line5 = "is not";
   }
 
+  var startDay = moment();
+  var currentTime = moment().startOf('day');
+  var secondsInDay = startDay.diff (currentTime, 'seconds');
 
 function dateFactor (){
-  console.log('It is ' + chalk.blue(`${moment()})`)+".");
+  console.log('It is ' + chalk.blue(`${moment()}`)+".");
   console.log(`It is` + chalk.magenta(` ${moment().dayOfYear()}th`) + " day of the year." );
-  console.log('It is' + chalk.green(`${moment().unix(Number)}`) + "seconds into the day.");
+  console.log('It is ' + chalk.green(`${secondsInDay}`) + " seconds into the day.");
   console.log('It'+ chalk.green(` ${line4}`) + " during daylight savings time.") ;
   console.log('It' + chalk.red(` ${line5}`) + " a leap year."  ) ;
 }
